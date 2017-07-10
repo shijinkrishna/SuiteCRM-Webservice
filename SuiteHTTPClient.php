797b1aeb9145eee46bd10bcb58901288210cbbf3
Shijin Krishna <shijin1323@gmail.com>
2017-07-10 17:47:06 +0530

    SuiteHTTP Client Class

---
+++ SuiteHTTPClient.php
@@ -0,0 +1,53 @@
+<?php
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+/**
+ * Very basic HTTP client
+ * @api
+ * Used in various places of the code and can be mocked out.
+ * Presently does only one op - POST to url.
+ */
+class SuiteHttpClient
+{
+    protected $last_error = '';
+    /**
+     * sends POST request to REST service via CURL
+     * @param string $url URL to call
+     * @param string $postArgs POST args
+     */
+    public function call($url, $postArgs)
+    {
+        if(!function_exists("curl_init")) {
+            $this->last_error = 'ERROR_NO_CURL';         
+            return false;
+        }
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $postArgs);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        $GLOBALS['log']->debug("HTTP client call: $url -> $postArgs");
+        $response = curl_exec($curl);
+        if($response === false) {
+            $this->last_error = 'ERROR_REQUEST_FAILED';
+            $curl_errno = curl_errno($curl);
+            $curl_error = curl_error($curl);
+            return false;
+        }
+
+        curl_close($curl);
+        return $response;
+    }
+
+    /**
+     * Returns code of last error that happened to the client
+     * @return string
+     */
+    public function getLastError()
+    {
+        return $this->last_error;
+    }
+}
+
